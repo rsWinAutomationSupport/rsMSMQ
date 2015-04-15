c777f61077c90db46382106f4f86553c9613a3ca
Mark Key <mark.key@rackspace.com>
2015-04-15 13:47:22 -0500

    new resource

---
+++ DSCResources/RS_rsSendMessage/RS_rsSendMessage.psm1
@@ -0,0 +1,60 @@
+﻿Function Get-TargetResource {
+  param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Name,
+    [string]$DestinationQueue,
+    [string]$MessageDestination,
+    [string]$MessageLabel,
+    [hashtable]$MessageBody,
+    [string]$Ensure
+  )
+  return @{
+    'DestinationQueue' = $DestinationQueue
+    'MessageDestination' = $MessageDestination
+    'MessageLabel' = $MessageLabel
+    'MessageBody' = $MessageBody
+    'Name' = $Name
+    'Ensure' = $Ensure
+  }
+}
+
+Function Test-TargetResource {
+  param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Name,
+    [string]$DestinationQueue,
+    [string]$MessageDestination,
+    [string]$MessageLabel,
+    [hashtable]$MessageBody,
+    [string]$Ensure
+  )
+  return $false
+}
+
+Function Set-TargetResource {
+  param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Name,
+    [string]$DestinationQueue,
+    [string]$MessageDestination,
+    [string]$MessageLabel,
+    [hashtable]$MessageBody,
+    [string]$Ensure
+  )
+  if($Ensure -eq 'Present') {
+    [Reflection.Assembly]::LoadWithPartialName("System.Messaging") | Out-Null
+    $msg = New-Object System.Messaging.Message
+    $msg.Label = $MessageLabel
+    $msg.Body = $MessageBody
+    $queue = New-Object System.Messaging.MessageQueue ($DestinationQueue, $False, $False)
+    $queue.Send($msg)
+  }
+  else {
+    Write-Verbose "Not Sending Messages"
+  }
+}
+
+Export-ModuleMember -Function *-TargetResource
