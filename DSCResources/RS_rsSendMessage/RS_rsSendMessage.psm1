b69d2c31d8fa1294a01f746455c2e5bea72b5578
mark4990 <mark.key@rackspace.com>
2015-04-17 08:37:16 -0500

    update message

--- DSCResources/RS_rsSendMessage/RS_rsSendMessage.psm1
+++ DSCResources/RS_rsSendMessage/RS_rsSendMessage.psm1
@@ -1,59 +1,62 @@
 ﻿Function Get-TargetResource {
-  param (
-    [Parameter(Mandatory)]
-    [ValidateNotNullOrEmpty()]
-    [string]$Name,
-    [string]$DestinationQueue,
-    [string]$MessageLabel,
-    [string]$Ensure
-  )
-  return @{
-    'DestinationQueue' = $DestinationQueue
-    'MessageLabel' = $MessageLabel
-    'Name' = $Name
-    'Ensure' = $Ensure
-  }
+   param (
+      [Parameter(Mandatory)]
+      [ValidateNotNullOrEmpty()]
+      [string]$Name,
+      [string]$DestinationQueue,
+      [string]$MessageLabel,
+      [string]$Ensure
+   )
+   return @{
+      'DestinationQueue' = $DestinationQueue
+      'MessageLabel' = $MessageLabel
+      'Name' = $Name
+      'Ensure' = $Ensure
+   }
 }
 
 Function Test-TargetResource {
-  param (
-    [Parameter(Mandatory)]
-    [ValidateNotNullOrEmpty()]
-    [string]$Name,
-    [string]$DestinationQueue,
-    [string]$MessageLabel,
-    [string]$Ensure
-  )
-  return $false
+   param (
+      [Parameter(Mandatory)]
+      [ValidateNotNullOrEmpty()]
+      [string]$Name,
+      [string]$DestinationQueue,
+      [string]$MessageLabel,
+      [string]$Ensure
+   )
+   return $false
 }
 
 Function Set-TargetResource {
-  param (
-    [Parameter(Mandatory)]
-    [ValidateNotNullOrEmpty()]
-    [string]$Name,
-    [string]$DestinationQueue,
-    [string]$MessageLabel,
-    [string]$Ensure
-  )
-  $bootstrapinfo = Get-Content "C:\Windows\Temp\bootstrapinfo.json" -Raw | ConvertFrom-Json
-  if($Ensure -eq 'Present') {
-     [Reflection.Assembly]::LoadWithPartialName("System.Messaging") | Out-Null
-     $msg = New-Object System.Messaging.Message
-     $msg.Label = $MessageLabel
-     $msg.Body = @{
-     "Name" = $env:COMPUTERNAME;
-     "uuid" = $bootstrapinfo.MyGuid;
-     "dsc_config" = $bootstrapinfo.dsc_config;
-     "shared_key" = $bootstrapinfo.shared_key;
-     "PublicCert" = $([System.Convert]::ToBase64String($((Get-ChildItem Cert:\LocalMachine\Root | ? Subject -eq "CN=$env:COMPUTERNAME`_enc").RawData)))
-        }
-    $queue = New-Object System.Messaging.MessageQueue ($DestinationQueue, $False, $False)
-    $queue.Send($msg)
-  }
-  else {
-    Write-Verbose "Not Sending Messages"
-  }
+   param (
+      [Parameter(Mandatory)]
+      [ValidateNotNullOrEmpty()]
+      [string]$Name,
+      [string]$DestinationQueue,
+      [string]$MessageLabel,
+      [string]$Ensure
+   )
+   $bootstrapinfo = Get-Content "C:\Windows\Temp\bootstrapinfo.json" -Raw | ConvertFrom-Json
+   if($Ensure -eq 'Present') {
+      $bootstrapinfo = Get-Content "C:\Windows\Temp\bootstrapinfo.json" -Raw | ConvertFrom-Json
+      [Reflection.Assembly]::LoadWithPartialName("System.Messaging") | Out-Null
+      $publicCert = ((Get-ChildItem Cert:\LocalMachine\Root | ? Subject -eq "CN=$env:COMPUTERNAME`_enc").RawData)
+      $msgbody = @{'Name' = "$env:COMPUTERNAME"
+         'uuid' = $($bootstrapinfo.MyGuid)
+         'dsc_config' = $($bootstrapinfo.dsc_config)
+         'shared_key' = $($bootstrapinfo.shared_key)
+         'PublicCert' = "$([System.Convert]::ToBase64String($publicCert))"
+      } | ConvertTo-Json
+      $msg = New-Object System.Messaging.Message
+      $msg.Label = 'execute'
+      $msg.Body = $msgbody
+      $queueName = "FormatName:DIRECT=HTTPS://$($bootstrapinfo.Name)/msmq/private$/rsdsc"
+      $queue = New-Object System.Messaging.MessageQueue ($queueName, $False, $False)
+      $queue.Send($msg)
+   }
+   else {
+      Write-Verbose "Not Sending Messages"
+   }
 }
 
 
